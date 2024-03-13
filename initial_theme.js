87bd74d0d285f08cb495f39456ecf7e04ef4e39d
Alex O'Hara <awostorm@gmail.com>
2024-03-13 16:04:09 -0600

    everything lol

---
+++ initial_theme.js
@@ -0,0 +1,16 @@
+import themes from "/Data/color_themes.json" assert { type: "json"};
+
+const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+let cur_theme = "darkest"
+
+if (darkTheme.matches) {
+    cur_theme = "darkest";
+}
+else {
+    cur_theme = "lightest";
+}
+
+const theme = themes[cur_theme];
+
+document.getElementById("main_body").style = "background-color: " + theme.secondary;
+document.getElementById("page_style").href = "css/page_styles_for_themes/style_" + cur_theme + ".css";
